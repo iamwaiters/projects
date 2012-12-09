ab2ca6d91c70b801e5c81fa34b8228dd0fa86ae4
iamric <iamwaiters@yahoo.com>
2012-12-09 11:07:53 -0600

    Java to C# test

---
+++ workspace/ZooObjects/ZooObjects/FourLeggedAnimal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZooObjects
+{
+    abstract class FourLeggedAnimal
+    {
+
+        public virtual string Describe()
+        {
+            return "NOt much is know about this four legged animal!";
+        }
+    }
+}
---
+++ workspace/ZooObjects/ZooObjects/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZooObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animals animal = new Animals();
+            animal.Greet();
+            Dog dog = new Dog();
+            dog.Greet();
+            Console.WriteLine(dog.Describe());
+        }
+    }
+}
